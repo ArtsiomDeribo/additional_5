e4302f1931559f131a3fa7b1ff43c71d6253377f
ArtsiomDeribo <31903188+ArtsiomDeribo@users.noreply.github.com>
2017-09-13 15:29:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var counter = 0;
+  for (var i = 1; i <= str.length; i++){
+    for (var a = 0; a < bracketsConfig.length(a); a++){
+      for (var b = 0; b < bracketsConfig.length(b); b++){
+        if (str.charAt(i) == bracketsConfig[a][0]{
+            counter++;
+            } else {
+            counter--;
+        }
+      }
+    }
+  }
+  if (counter == 0){
+    return true;
+        } else {
+          return false;
+        }
+      
+           
 }
